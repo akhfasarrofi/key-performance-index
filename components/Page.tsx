8f472c78918ffe150eba0a63098bf8e251478979
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2022-10-13 20:57:19 +0700

    reusable page components

---
+++ components/Page.tsx
@@ -0,0 +1,28 @@
+// import { Helmet } from 'react-helmet-async';
+import { forwardRef, ReactNode, Ref } from 'react';
+import Box from '@mui/material/Box';
+
+interface PageProps {
+    children?: ReactNode
+    title: string
+    meta?: ReactNode
+}
+
+const Page = forwardRef(function Page(
+    props: PageProps,
+    ref: Ref<unknown>,
+) {
+    const { children, title = '', meta, ...other } = props;
+    return (
+        <>
+            <title>{`${title} | Key Performance Index`}</title>
+            {meta}
+
+            <Box ref={ref} {...other}>
+                {children}
+            </Box>
+        </>
+    );
+});
+
+export default Page;
