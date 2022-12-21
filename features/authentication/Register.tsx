282ecce0ad4d6603abbb10ccb4eff91b9f0f9378
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2022-12-21 21:40:05 +0700

    update

---
+++ features/authentication/Register.tsx
@@ -0,0 +1,70 @@
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Stack, IconButton, InputAdornment } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import Iconify from 'components/Iconify';
+import { FormProvider, RHFTextField } from 'hooks/hook-form';
+import { useRouter } from 'next/router';
+
+export default function RegisterForm() {
+    const router = useRouter();
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const RegisterSchema = Yup.object().shape({
+        firstName: Yup.string().required('First name required'),
+        lastName: Yup.string().required('Last name required'),
+        email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+        password: Yup.string().required('Password is required'),
+    });
+
+    const defaultValues = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+    };
+
+    const methods = useForm({
+        resolver: yupResolver(RegisterSchema),
+        defaultValues,
+    });
+
+    const {handleSubmit, formState: { isSubmitting }} = methods;
+
+    const onSubmit = async () => router.push('/');
+
+    return (
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={3}>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                    <RHFTextField name="firstName" label="First name" />
+                    <RHFTextField name="lastName" label="Last name" />
+                </Stack>
+
+                <RHFTextField name="email" label="Email address" />
+
+                <RHFTextField
+                    name="password"
+                    label="Password"
+                    type={showPassword ? 'text' : 'password'}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton edge="end" onClick={() => setShowPassword(!showPassword)}>
+                                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+
+                <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={isSubmitting}>
+                    Register
+                </LoadingButton>
+            </Stack>
+        </FormProvider>
+    );
+}
